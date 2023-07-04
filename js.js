29bf37f8e1b7bb65d3a289f998425cb4074bdb75
Sana-K84 <54663549+Sana-K84@users.noreply.github.com>
2023-07-05 00:39:44 +0300

    +

---
+++ js.js
@@ -0,0 +1,25 @@
+const tabsTitle = document.querySelectorAll('.tab_title');
+const tabsContent = document.querySelectorAll('.content');
+console.log(tabsTitle)
+
+tabsTitle.forEach(el => el.addEventListener('click', event => {
+    const tabsTitleTarget = event.target.getAttribute('data-tab');
+
+
+
+
+    tabsTitle.forEach(el => el.classList.remove('activ'));
+    if (tabsTitleTarget === 'All') {
+        tabsContent.forEach(el => el.classList.remove('hidden'));
+    } else { tabsContent.forEach(el => el.classList.add('hidden')); }
+
+
+    el.classList.add('activ')
+    const tabsContentHidden = document.getElementsByClassName(tabsTitleTarget)
+
+    for (elem of tabsContentHidden) {
+        elem.classList.remove('hidden')
+    }
+
+}))
+
